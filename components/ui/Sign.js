91a07f52e0e74f71bd2107a3a23e58ee4ade8b84
Rehema-Nanda <developer@kujakuja.com>
2023-03-29 13:46:53 +0300

    change style

--- components/ui/Sign.js
+++ components/ui/Sign.js
@@ -36,36 +36,5 @@ export default SignComponent;
 
 
 
-// const SignComponent = () => {
-//   const [signature, setSignature] = useState(null);
-//   const canvasRef = useRef(null);
-
-//   const handleClear = () => {
-//     canvasRef.current.clear();
-//   };
-
-//   const handleSave = () => {
-//     const dataUrl = canvasRef.current.toDataURL();
-//     localStorage.setItem('signature', dataUrl);
-//   };
-
-//   return (
-//     <>
-//       <div className="signature-container">
-//         <SignatureCanvas
-//           canvasProps={{ className: "sig-canvas" }}
-//           ref={canvasRef}
-//           onEnd={() => setSignature(canvasRef.current.toDataURL())}
-//         />
-//       </div>
-//       <div className="button-container">
-//         <button className="button-clear" onClick={handleClear}>Clear</button>
-//         <button className="button-save" onClick={handleSave}>Save</button>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default SignComponent;
 
 
